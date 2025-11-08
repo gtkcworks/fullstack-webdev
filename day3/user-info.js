db0aa5a17231ec0fd2b9bf025460dab739910cd8
GTKC <gtkc.works@gmail.com>
2025-11-09 03:15:06 +0800

    Create user-info.js

---
+++ day3/user-info.js
@@ -0,0 +1,11 @@
+//Create a file user-info.js:
+
+//Write an async function getUser(id)
+
+//Fetch user data from: https://jsonplaceholder.typicode.com/users/${id}
+
+//Use try...catch to handle errors
+
+//Log the user's name and email
+
+//Test it with different IDs (1, 2, 10, etc.)
