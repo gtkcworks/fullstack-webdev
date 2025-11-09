7ee9cf6b245a3c54ec292f96d0770e0b80c3304a
GTKC <gtkc.works@gmail.com>
2025-11-10 03:40:12 +0800

    day4

---
+++ day4/my-react-app/src/myinfo.jsx
@@ -0,0 +1,15 @@
+function Myinfo() {
+  return (
+    <div>
+      <h1>Gordon Tai</h1>
+      <p>Aspiring WebDev</p>
+      <ul>
+        <li>Chess</li>
+        <li>Badminton</li>
+        <li>Psychology</li>
+      </ul>
+    </div>
+  );
+}
+
+export default Myinfo;
